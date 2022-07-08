d6896ebd6f2c1fb9d4c45cc05466652e16934895
Iñigo Gallego <igallego@ecna.es->
2022-07-08 13:59:28 +0200

    first commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent {
+  heroes: string[]=['Spiderman','Ironman','Hulk', 'Thor', 'Capitán America']
+  heroeBorrado: string  = '';
+  borrarHeroe(){
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+}
